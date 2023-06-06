a1dd00d1b0d172c002a85cc14c35895bdec6bf6f
Swathy96 <swathycool01@gmail.com>
2023-06-05 17:25:57 -0700

    Implementing search bar

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from "react";
 const App = ()=>{
   const [news,setNews] = useState([])
 
+  const [searchTerm, setSearchTerm] = useState('react')
+
   const fetchNews = ()=>{
-    fetch('https://hn.algolia.com/api/v1/search?query=react')
+    fetch(`https://hn.algolia.com/api/v1/search?query=${searchTerm}`)
     .then( result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error))
@@ -12,11 +14,19 @@ const App = ()=>{
 
   useEffect(()=>{
     fetchNews();
-  })
+  },[searchTerm])
+
+  const handleChange = (e) =>{
+    setSearchTerm(e.target.value)
+  }
 
   return(
     <div>
       <h2>News</h2>
+      <form>
+        <input type="search"  value={searchTerm} onChange={handleChange}/>
+        <button>Search</button>
+      </form>
       {news.map((n, index) => (<p key={index}>{n.title}</p>))}
     </div>
   )
